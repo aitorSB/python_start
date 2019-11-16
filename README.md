0c7b0ca0ab7bb18e8479964a53262d0952c2e933
aitor <aitorsan2092@gmail.com>
2019-11-16 10:13:40 +0100

    first commit Introducción python

---
+++ README.md
@@ -0,0 +1,66 @@
+
+# Ejecutar el fichero en consola
+
+> python  ./start.py
+
+# Abrir la consola de python
+
+>python 
+
+>py
+
+## Salir de la consola
+
+>exit()
+
+# Imprimir
+
+```py
+print("Hello, World!")
+```
+
+# Comentarios
+
+> #Esto es un comentario de una linea.
+
+```py
+#Comentario
+print("Hello, World!") #Esto es un comentario
+```
+
+> """ comentario en más de una linea """
+
+```py
+"""
+esto es un comentario en 
+diferentes lineas
+"""
+````
+
+# Indentación
+
+```py
+if 5 > 2:
+  print("Five is greater than two!")
+```
+
+# Variables
+
+```py
+x = 5
+
+# Las variables string puedes ser declaradas con comillas simples o dobles
+y = "Hellow, World!"
+
+print(y)
+```
+
+## Forma de dar valor a las variables
+
+```py
+x1, y2 , z3 = "pera", "manzana", "platano"
+
+print(x1)
+print(y2)
+print(z3)
+```
---
+++ start.py
@@ -0,0 +1,34 @@
+#! python3
+
+# *************************************************************************
+# imprimir en consola
+# *************************************************************************
+
+print("Hello, World!")
+
+# *************************************************************************
+# indentacion
+# *************************************************************************
+
+if 5 > 2:
+    print('5 es mayor que 2')
+
+# *************************************************************************
+# variables
+# *************************************************************************
+
+x = 5 #variable int
+y = "Hello, World!" #variable string comillas dobles
+z = 'Hellow, World2!' #variable string comillas simples
+
+print(x)
+print(y)
+print(z)
+    
+## distinta fdrma de dar valor a las variables
+
+x1, y2 , z3 = "pera", "manzana", "platano"
+
+print(x1)
+print(y2)
+print(z3)
